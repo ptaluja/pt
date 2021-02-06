0313c3d30e241b34ff08411fbe5e1bf464af1f7a
ptaluja <77400378+ptaluja@users.noreply.github.com>
2021-02-07 02:26:17 +0530

    v

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ const express=require("express");
 const serverless=require("serverless-http");
 const app=express();
 const ejs=require('ejs');
+const path=require('path');
 app.engine('ejs', require('ejs').__express);
 app.set('view engine','ejs');
 app.get('/.netlify/functions/api', function(req, res) {
