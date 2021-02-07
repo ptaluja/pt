a7ea56d0c99c313762c7aadd45466e0083c65eef
ptaluja <77400378+ptaluja@users.noreply.github.com>
2021-02-07 12:20:45 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,11 @@
-
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+app.engine('ejs', require('ejs').__express);
+app.set('view engine', 'ejs');
+app.get('/.netlify/functions/api', function(req, res) {
+    res.render('main');
+});
+module.exports.handler = serverless(app);
